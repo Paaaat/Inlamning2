577f2761fa9862f2760e88e7a5fd684f9fa618c9
Patrik Larsson <Patrik.Larsson@yh.nackademin.se>
2016-11-07 14:39:07 +0100

    dfgh

--- Inlamning2/ConsoleInalmning2/Program.cs
+++ Inlamning2/ConsoleInalmning2/Program.cs
@@ -14,106 +14,118 @@ namespace ConsoleInalmning2
         {
             string selected = "";
             Console.WriteLine("1: Insert cutsomer \n2: Insert product \n3: Update product price \n(Select with nummbers)");
-
-            int selection = int.Parse(Console.ReadLine());
-
-            if (selection == 1)
+            try
             {
-                Console.WriteLine("CustomerID: ");
-                string ID = Console.ReadLine();
-                Console.WriteLine("Company name: ");
-                string companyName = Console.ReadLine();
-                Console.WriteLine("Contact name: ");
-                string contactName = Console.ReadLine();
-                Console.WriteLine("Phonenumber");
-                string phone = Console.ReadLine();
-                try
-                {
-                    string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
-                    SqlConnection cn = new SqlConnection(cns);
-                    cn.Open();
-                    SqlCommand cmd = cn.CreateCommand();
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    cmd.CommandText = "InsertCustomers";
-                    cmd.Parameters.AddWithValue("@CustomerID", ID);
-                    cmd.Parameters.AddWithValue("@CompanyName", companyName);
-                    cmd.Parameters.AddWithValue("@ContactName", contactName);
-                    cmd.Parameters.AddWithValue("@Phone", phone);
-                    cmd.ExecuteNonQuery();
-                    cn.Close();
-                    Console.WriteLine("Customer " + contactName + " was added");
-                }
-                catch
-                {
-                    Console.WriteLine("Customer with that ID already exists");
-                }
-            }
-            else if (selection == 2)
-            {
-                Console.WriteLine("Product name: ");
-                string productName = Console.ReadLine();
-                Console.WriteLine("Unit price: ");
-                string unitPrice = Console.ReadLine();
+                int selection = int.Parse(Console.ReadLine());
 
-                try
+                if (selection > 3)
                 {
-                    string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
-                    SqlConnection cn = new SqlConnection(cns);
-                    cn.Open();
-                    SqlCommand cmd = cn.CreateCommand();
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    cmd.CommandText = "InsertProducts";
-                    cmd.Parameters.AddWithValue("@ProductName", productName);
-                    cmd.Parameters.AddWithValue("@UnitPrice", unitPrice);
-                    cmd.ExecuteNonQuery();
-                    cn.Close();
-                    Console.WriteLine("Product: " + productName + " was added");
+                    Console.WriteLine("Select a number between 1-3");
                 }
-                catch
+
+                if (selection == 1)
                 {
-                    Console.WriteLine("Something went wrong");
-                }
-            }
-            else if (selection == 3)
-            {
-                try
-                {
-                    using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes"))
-                    using (SqlCommand command = connection.CreateCommand())
+                    Console.WriteLine("CustomerID: ");
+                    string ID = Console.ReadLine();
+                    Console.WriteLine("Company name: ");
+                    string companyName = Console.ReadLine();
+                    Console.WriteLine("Contact name: ");
+                    string contactName = Console.ReadLine();
+                    Console.WriteLine("Phonenumber");
+                    string phone = Console.ReadLine();
+                    try
                     {
-                        command.CommandText = "select ProductID, ProductName from Products";
-                        connection.Open();
-                        using (var reader = command.ExecuteReader())
+                        string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
+                        SqlConnection cn = new SqlConnection(cns);
+                        cn.Open();
+                        SqlCommand cmd = cn.CreateCommand();
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.CommandText = "InsertCustomers";
+                        cmd.Parameters.AddWithValue("@CustomerID", ID);
+                        cmd.Parameters.AddWithValue("@CompanyName", companyName);
+                        cmd.Parameters.AddWithValue("@ContactName", contactName);
+                        cmd.Parameters.AddWithValue("@Phone", phone);
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                        Console.WriteLine("Customer " + contactName + " was added");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Customer with that ID already exists");
+                    }
+                }
+                else if (selection == 2)
+                {
+                    Console.WriteLine("Product name: ");
+                    string productName = Console.ReadLine();
+                    Console.WriteLine("Unit price: ");
+                    string unitPrice = Console.ReadLine();
+
+                    try
+                    {
+                        string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
+                        SqlConnection cn = new SqlConnection(cns);
+                        cn.Open();
+                        SqlCommand cmd = cn.CreateCommand();
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.CommandText = "InsertProducts";
+                        cmd.Parameters.AddWithValue("@ProductName", productName);
+                        cmd.Parameters.AddWithValue("@UnitPrice", unitPrice);
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                        Console.WriteLine("Product: " + productName + " was added");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Something went wrong");
+                    }
+                }
+                else if (selection == 3)
+                {
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes"))
+                        using (SqlCommand command = connection.CreateCommand())
                         {
-                            while (reader.Read())
+                            command.CommandText = "select ProductID, ProductName from Products";
+                            connection.Open();
+                            using (var reader = command.ExecuteReader())
                             {
-                                Console.WriteLine(reader.GetInt32(0) + ": " + (reader.GetString(1)));
+                                while (reader.Read())
+                                {
+                                    Console.WriteLine(reader.GetInt32(0) + ": " + (reader.GetString(1)));
+                                }
                             }
                         }
+
+                        Console.WriteLine("Select the ID of the product you want to edit");
+                        selected = Console.ReadLine();
+                        Console.WriteLine("Select the new Price for the product: ");
+                        string newPrice = Console.ReadLine();
+
+                        string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
+                        SqlConnection cn = new SqlConnection(cns);
+                        cn.Open();
+                        SqlCommand cmd = cn.CreateCommand();
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.CommandText = "ProductsUpdate";
+                        cmd.Parameters.AddWithValue("@ProductID", selected);
+                        cmd.Parameters.AddWithValue("@UnitPrice", newPrice);
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                        Console.WriteLine("Price changed to:" + newPrice);
                     }
-
-                    Console.WriteLine("Select the ID of the product you want to edit");
-                    selected = Console.ReadLine();
-                    Console.WriteLine("Select the new Price for the product: ");
-                    string newPrice = Console.ReadLine();
-
-                    string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
-                    SqlConnection cn = new SqlConnection(cns);
-                    cn.Open();
-                    SqlCommand cmd = cn.CreateCommand();
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    cmd.CommandText = "ProductsUpdate";
-                    cmd.Parameters.AddWithValue("@ProductID", selected);
-                    cmd.Parameters.AddWithValue("@UnitPrice", newPrice);
-                    cmd.ExecuteNonQuery();
-                    cn.Close();
-                    Console.WriteLine("Price changed to:" + newPrice);
-                }
-                catch
-                {
-                    Console.WriteLine("Something went wrong");
+                    catch
+                    {
+                        Console.WriteLine("Something went wrong");
+                    }
                 }
             }
+            catch
+            {
+                Console.WriteLine("Select a valid number");
+            }
+            Console.ReadLine();
         }
     }
 }
